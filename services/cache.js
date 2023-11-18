46ece86b7f69bf220b83c51d6a1ef315eb577dbc
root <root@Flow-X13>
2023-11-18 16:46:02 +0100

    we set up a caching system with Redis

---
+++ services/cache.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = "redis://127.0.0.1:6379";
+const client = redis.createClient(redisUrl);
+client.hget = util.promisify(client.hget);
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = async function () {
+    if (!this.useCache) {
+        return exec.apply(this, arguments);
+    }
+    const key = JSON.stringify(Object.assign({}, this.getQuery(), {
+        collection: this.mongooseCollection.name,
+    }));
+
+    // See if we have a value for "key" in redis
+    const cachedValue = await client.hget(this.hashKey, key);
+
+    if (cachedValue) {
+        const doc = JSON.parse(cachedValue);
+        return Array.isArray(doc) ? doc.map(d => new this.model(d)) : new this.model(JSON.parse(cachedValue))
+    }
+
+    const res = await exec.apply(this, arguments);
+
+    await client.hset(this.hashKey, key, JSON.stringify(res), 'EX', 10);
+    
+    return res;
+}
+
+mongoose.Query.prototype.cache = function(options = {}) {
+    this.useCache = true;
+    this.hashKey = JSON.stringify(options.key || '');
+    return this;
+}
+
+module.exports = {
+    clearHash(hashKey) {
+        client.del(JSON.stringify(hashKey));
+    }
+}
